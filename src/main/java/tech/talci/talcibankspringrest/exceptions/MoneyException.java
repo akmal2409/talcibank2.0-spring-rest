68df1ffd614d1994979883668283c50857ddabf8
akmal2409 <akmalalikhujaev@gmail.com>
2020-08-26 14:44:20 +0200

    Created two custome exceptions (MoneyException and ResourceNotFoundException) to hide internal difficulties from the user

--- src/main/java/tech/talci/talcibankspringrest/exceptions/MoneyException.java
+++ src/main/java/tech/talci/talcibankspringrest/exceptions/MoneyException.java
@@ -1,4 +1,24 @@
 package tech.talci.talcibankspringrest.exceptions;
 
-public class MoneyException {
+public class MoneyException extends RuntimeException{
+
+    public MoneyException() {
+        super();
+    }
+
+    public MoneyException(String message) {
+        super(message);
+    }
+
+    public MoneyException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public MoneyException(Throwable cause) {
+        super(cause);
+    }
+
+    protected MoneyException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
 }
