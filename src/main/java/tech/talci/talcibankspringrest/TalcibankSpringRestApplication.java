f4564c7fa13d3c7bdee54ffae62652ba72ee3d32
akmal2409 <akmalalikhujaev@gmail.com>
2020-08-26 12:50:12 +0200

    Initial Commit

---
+++ src/main/java/tech/talci/talcibankspringrest/TalcibankSpringRestApplication.java
@@ -0,0 +1,13 @@
+package tech.talci.talcibankspringrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TalcibankSpringRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TalcibankSpringRestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tech/talci/talcibankspringrest/TalcibankSpringRestApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.talci.talcibankspringrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TalcibankSpringRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
