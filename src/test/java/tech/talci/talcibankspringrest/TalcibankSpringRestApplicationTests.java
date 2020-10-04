fa8ae176ce31659df550ee621eb5195cdcbc9d75
akmal2409 <akmalalikhujaev@gmail.com>
2020-10-04 20:36:42 +0200

    Fixed application properties

--- src/test/java/tech/talci/talcibankspringrest/TalcibankSpringRestApplicationTests.java
+++ src/test/java/tech/talci/talcibankspringrest/TalcibankSpringRestApplicationTests.java
@@ -4,9 +4,11 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
 @EnableAutoConfiguration(exclude = { SecurityAutoConfiguration.class })
+@TestPropertySource(locations="classpath:application-dev.properties")
 class TalcibankSpringRestApplicationTests {
 
     @Test
