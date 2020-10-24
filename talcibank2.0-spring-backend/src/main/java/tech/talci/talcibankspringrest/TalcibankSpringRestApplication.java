6a2268d596a2217c68471a0e671aac339c6ce230
akmal2409 <akmalalikhujaev@gmail.com>
2020-10-24 15:48:36 +0200

    Added login and signup forms

--- talcibank2.0-spring-backend/src/main/java/tech/talci/talcibankspringrest/TalcibankSpringRestApplication.java
+++ talcibank2.0-spring-backend/src/main/java/tech/talci/talcibankspringrest/TalcibankSpringRestApplication.java
@@ -2,10 +2,13 @@ package tech.talci.talcibankspringrest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 import org.springframework.scheduling.annotation.EnableAsync;
+import tech.talci.talcibankspringrest.config.SwaggerConfiguration;
 
 @SpringBootApplication
 @EnableAsync
+@Import(SwaggerConfiguration.class)
 public class TalcibankSpringRestApplication {
 
     public static void main(String[] args) {
--- talcibank2.0-spring-backend/src/main/java/tech/talci/talcibankspringrest/config/SecurityConfig.java
+++ talcibank2.0-spring-backend/src/main/java/tech/talci/talcibankspringrest/config/SecurityConfig.java
@@ -29,6 +29,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .authorizeRequests()
                 .antMatchers("/api/auth/**")
                 .permitAll()
+                .antMatchers("/v2/api-docs",
+                        "/configuration/ui",
+                        "/swagger-resources/**",
+                        "/configuration/security",
+                        "/swagger-ui.html",
+                        "/webjars/**")
+                .permitAll()
                 .antMatchers("/api/users/{userId}/**")
                 .access("@userSecurity.hasUserId(authentication, #userId)")
                 .anyRequest()
